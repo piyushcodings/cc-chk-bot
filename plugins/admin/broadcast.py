09be23f824bceb0c363a2dc5305b2dac62e2f76a
piyushcodings <118120770+piyushcodings@users.noreply.github.com>
2023-03-11 17:10:36 +0530

    Update broadcast.py

--- plugins/admin/broadcast.py
+++ plugins/admin/broadcast.py
@@ -3,8 +3,8 @@ from plugins.func.users_sql import *
 @Client.on_message(filters.command ('brod'))
 async def cmd_brod(Client,message):
   user_id = str(message.from_user.id)
-  CEO = "1900986195"
-  owner = 1900986195
+  CEO = "5748100919"
+  owner = 5748100919
   if user_id != CEO :
     resp = "𝗥𝗲𝗾𝘂𝗶𝗿𝗲 𝗢𝘄𝗻𝗲𝗿 𝗣𝗿𝗶𝘃𝗶𝗹𝗮𝗴𝗲𝘀 ⚠️"
     msg1 = await message.reply_text(resp,message.id)
